a39cbfb14eb433a6dafa49558190a19453bb98fd
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-08-22 20:31:14 +0500

    feat: add initial variables.js file with ES6 let explanation and variable usage

---
+++ m31/variables.js
@@ -0,0 +1,8 @@
+
+
+// The Datatype let was introduced in ES6(ECMA Script 2015), released in 2015 
+// We avoid var in modern JavaScript because it introduces unpredictable behavior, lacks block-scoping, and silently allows duplicate declarations, which can cause hard-to-find bugs
+
+var name="Khubaib"
+name = "Adeel"
+console.log(name)
