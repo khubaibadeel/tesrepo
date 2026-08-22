78970b2b807e28f06ccceba3a344b5ed497c798c
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-08-22 20:36:12 +0500

    feat: migrate variables.js content to v.js and update comments for clarity

--- m31/v.js
+++ m31/v.js
@@ -3,6 +3,11 @@
 // The Datatype let was introduced in ES6(ECMA Script 2015), released in 2015 
 // We avoid var in modern JavaScript because it introduces unpredictable behavior, lacks block-scoping, and silently allows duplicate declarations, which can cause hard-to-find bugs
 
-var name="Khubaib"
-name = "Adeel"
-console.log(name)
+// var name="Khubaib"
+// name = "Adeel"
+// console.log(name)
+
+// let name="Khubaib"
+// name="Adeel"
+// console.log(name)
+
