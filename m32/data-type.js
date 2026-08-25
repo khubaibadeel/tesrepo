ad717a3a750376e6c9446ad743d64d534b045545
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-08-25 22:44:43 +0500

    feat: add initial data type definitions and console logs

---
+++ m32/data-type.js
@@ -0,0 +1,19 @@
+
+// Primitive Data Types:   
+// string, numbers(int,float), boolean, undefined, null
+
+let name="Khubaib" //string
+let age=25         //int
+let height=1.8     //float
+let isStudent=true //boolean
+let test;          //undefined
+
+let city=null      //null (use null when u want to save nothing in variable)
+
+console.log(name)
+console.log(age)
+console.log(height)
+console.log(isStudent)
+console.log(test)
+console.log(city)
+
