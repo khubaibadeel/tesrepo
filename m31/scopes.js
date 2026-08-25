bc559b3b344dd1677c610d66addd7c728716c35a
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-08-25 17:00:46 +0500

    feat: reorganize scopes.js to clearly define global and functional scopes

--- m31/scopes.js
+++ m31/scopes.js
@@ -3,6 +3,23 @@
 //         --> Functional
 //         --> Block
         
+
+// Global Scope:
+
+let name="Khubaib"     // name is a Global Scope Variable. It can be called everywhere in the program.
+function greet(){
+    console.log(name);
+}
+greet();
+
+
+// Functional Scope:
+
+function greet1(){
+    let fathername="Adeel"  // fname is a Functional Scope Variable only. Can be called in the function only.
+    console.log(fathername);
+}
+greet1();
+
 // Temp Command to run this file in terminal using node:
 // node scopes.js
-
