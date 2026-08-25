49be880ccfecefe7d0f021c7dc532ba98c427975
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-08-25 23:07:17 +0500

    feat: add non-primitive data type example file

---
+++ m33/non-premitive-data-type.js
@@ -0,0 +1,13 @@
+
+// Syntax:
+// var_type={key:value, key:value,...}
+
+let user={ name:"Khubaib", age : 16, gender: "Male" }
+console.log(user)
+
+console.log(user.name)     // Recommended way to access the value
+// OR
+console.log(user["age"])
+
+// Temp command to run the code
+// node non-premitive-data-type.js
