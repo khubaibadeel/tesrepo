046b37b579c6c1b5b4084131219a0f05df808bc6
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-08-25 17:06:06 +0500

    feat: add examples of let and var variable declarations in compare-var-and-let.js

---
+++ m31/compare-var-and-let.js
@@ -0,0 +1,16 @@
+// let Example:
+
+function greet(){
+    let name="Khubaib"
+    console.log(name);
+}
+greet();
+
+
+// var Example:
+
+function greet1(){
+    var fathername="Adeel" 
+    console.log(fathername);
+}
+greet1();
