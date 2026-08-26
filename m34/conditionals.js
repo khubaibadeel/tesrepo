01e82eefd431c181075ba41f0636d62e7b7d70b2
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-08-26 09:23:27 +0500

    fix: update comments for ternary operator explanation in conditionals.js

--- m34/conditionals.js
+++ m34/conditionals.js
@@ -13,7 +13,11 @@ else {
 
 
 // Ternary Operator:
-// if(){}else{}
+//      "A ternary operator is a short way to write a simple if...else."
+
+// Syntax:
+//      condition ? valueIfTrue : valueIfFalse
+
 let marks=40
 let ispassorfail = marks>=40 ? "Pass" : "Fail"
 console.log(ispassorfail)
