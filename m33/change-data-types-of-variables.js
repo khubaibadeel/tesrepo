11d7d6b981a7e3fccad758fd51c9da67f887cac7
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-08-26 07:00:12 +0500

    uncomment example code for data type conversion

--- m33/change-data-types-of-variables.js
+++ m33/change-data-types-of-variables.js
@@ -2,18 +2,18 @@
 
 // console.log(typeof var_name)
 
-// let age = "16" 
-// console.log(typeof age)     // Output: string
+let age = "16" 
+console.log(typeof age)     // Output: string
 
-// age = Number(age)
-// console.log(typeof age)     // Output: number
+age = Number(age)
+console.log(typeof age)     // Output: number
 
 
-// let f_age = 45
-// console.log(typeof f_age)   // Output: number
+let f_age = 45
+console.log(typeof f_age)   // Output: number
 
-// f_age = String(f_age)
-// console.log(typeof f_age)   // Output: string
+f_age = String(f_age)
+console.log(typeof f_age)   // Output: string
 
 
 let isStudent = "true"
