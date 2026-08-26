cf616696fa0fa2708977de9d7c759b25c92d05b8
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-08-26 07:53:57 +0500

    fix: improve formatting and spacing in operators.js

--- m34/operators.js
+++ m34/operators.js
@@ -12,16 +12,31 @@ let a=1
 let b=2
 console.log(a+b)    // Output: 3
 
+
 // Increment/Decrement:
 let x=1
 a++
 console.log(a)      // Output: 2
 
-//Assignment:
+
+// Assignment:
 let num=2           // Assigned the value '2' to variable 'num'
 
-//Comparison:
+
+// Comparison:
 const name = "Khubaib"
 let surname = "Adeel"
 console.log(name == surname)   // Output: false
 
+// const fname = "Khubaib"
+// let fsurname ="Khubaib"
+// console.log(fname == fsurname)   // Output: true
+
+let age_1= "25"
+let age_2 = 25
+console.log(age_1 == age_2)    // Output: true 
+                            // bcz values are same and datatype is not judged
+
+let f_age= "40"
+let m_age= 40
+console.log(f_age === m_age)    //
