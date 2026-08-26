960ccabfdb5870a823447a4ac3a25c165e0d2492
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-08-26 06:45:31 +0500

    fix: ensure consistent formatting and spacing in data type checking example

--- m33/change-data-types-of-variables.js
+++ m33/change-data-types-of-variables.js
@@ -2,6 +2,15 @@
 
 // console.log(typeof var_name)
 
-let age = "25"
-console.log(typeof age)  // Output: string
+let age = "25" 
+console.log(typeof age)     // Output: string
 
+age = Number(age)
+console.log(typeof age)     // Output: number
+
+
+let height = "5.5"
+console.log(typeof height)  // Output: string
+
+height = Number(height)
+console.log(typeof height)  // Output: number
