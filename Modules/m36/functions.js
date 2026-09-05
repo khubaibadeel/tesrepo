f79cfdc649944088ee49d99184b728cfed9762fb
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-09-05 12:23:44 +0500

    feat: implement greet and addition functions with example usage

---
+++ Modules/m36/functions.js
@@ -0,0 +1,17 @@
+
+
+function greet(){
+    console.log("hello")
+}
+greet()   // <-- Function called
+
+
+// Practice Function
+function addition(a,b){
+    return a+b
+}
+
+let x=100
+let y=900
+let sum=addition(x,y)
+console.log(sum)
