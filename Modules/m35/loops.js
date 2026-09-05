c6c91d6d02fa20b2ad8729e3b3d4475745833e2d
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-09-05 11:15:30 +0500

    fix: comment out basic example of for loop in loops.js

--- Modules/m35/loops.js
+++ Modules/m35/loops.js
@@ -10,9 +10,19 @@
 
 
 // Basic Example:
-for (let i=1;i<=10;i++)
-{
-    console.log("Hello World")
-}
+// for (let i=1;i<=10;i++)
+// {
+//     console.log("Hello World")
+// }
 
 // Practical Example: How to manage the data coming from the server in structure of Arrays
+
+let array=[1,2,3,4,5]
+for (let val of array){
+    console.log(val)
+}
+
+let obj = {name:"Khabaib", fname:"Adeel", age:16}
+for (let key in obj){
+    console.log(key, obj[key])
+}
