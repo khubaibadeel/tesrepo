d873ccb742c001e4e8accb97fa5750de9f05f5ae
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-09-05 13:59:27 +0500

    fix: add examples for normal functions and functional expressions

--- Modules/m37/advance-concepts-of-functions.js
+++ Modules/m37/advance-concepts-of-functions.js
@@ -7,3 +7,16 @@
 // Arrow functions are used in Modern JavaScript Development.
 
 
+// Normal Function:-
+function greet(name){
+    console.log("Hello " + name)
+}
+greet("Khubaib")
+
+
+// Functional Expression:-
+intro=function(name){
+    console.log("Are you "+name+"?")
+}
+intro("Adeel")
+
