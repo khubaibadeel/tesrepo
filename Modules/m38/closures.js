23627e7c5450e94fe5ebf6faf75f7c5705f2b5b4
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-09-05 20:25:56 +0500

    feat: enhance closures.js with detailed comments on lexical scope

--- Modules/m38/closures.js
+++ Modules/m38/closures.js
@@ -1,2 +1,16 @@
 // The Concept of Closures in MERN Stack is very Important that its even asked during the Interviews of the Students!
 
+
+// Lexical Scope:-
+// "Function can access the variables in its surroundings, based on where it is written in the code."
+
+function outer(){
+    let course="MERN"
+
+    function inner(){
+        console.log(course) // The Inner Function can still access the course variable which is declared in the outer variable. This is Lexical Scope.
+    }
+
+    inner() // outer function calls inner function which further prints course's value.
+}
+outer()
