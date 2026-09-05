b869c50cd13be696ca72477b375a4d8df6daa89c
khubaibadeel <muhammadkhubaibadeel@gmail.com>
2026-09-05 13:53:50 +0500

    feat: add documentation for arrow functions in ES6

---
+++ Modules/m37/advance-concepts-of-functions.js
@@ -0,0 +1,9 @@
+
+
+// Arrow Functions:
+// Arrow Function was Introduced in ES6 (ECMAScript 2015).
+
+// Used to simplify the syntax of the functions we define.
+// Arrow functions are used in Modern JavaScript Development.
+
+
